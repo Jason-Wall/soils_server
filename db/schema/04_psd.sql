7d33364444ea38321a870a6429ccf2b167d4ac63
Jason-Wall <jason.waldick@gmail.com>
2023-05-02 14:48:30 -0600

    naming for psd.sql

---
+++ db/schema/04_psd.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS psd CASCADE;
+CREATE TABLE psd (
+  id SERIAL PRIMARY KEY NOT NULL,
+  sample_id INTEGER REFERENCES samples(id),
+  serial VARCHAR(255),
+  test_date DATE,
+  results JSON NOT NULL
+);
